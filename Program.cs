d6a296e2f737f24cda54af3a62d9e2fd1c57b1a1
KYS <kkyyss1996@gmail.com>
2026-01-20 21:56:24 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+namespace FileManager
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var builder = WebApplication.CreateBuilder(args);
+
+      // Add services to the container.
+      builder.Services.AddAuthorization();
+
+      var app = builder.Build();
+
+      // Configure the HTTP request pipeline.
+      var options = new DefaultFilesOptions();
+      options.DefaultFileNames.Clear();
+      options.DefaultFileNames.Add("main.html");
+      app.UseDefaultFiles(options);
+      app.UseStaticFiles();
+      app.UseAuthorization();
+      app.UseHttpsRedirection();
+
+      /*            var summaries = new[]
+                  {
+                      "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+                  };*/
+
+      /*            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+                  {
+                      var forecast = Enumerable.Range(1, 5).Select(index =>
+                          new WeatherForecast
+                          {
+                              Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                              TemperatureC = Random.Shared.Next(-20, 55),
+                              Summary = summaries[Random.Shared.Next(summaries.Length)]
+                          })
+                          .ToArray();
+                      return forecast;
+                  });
+      */
+
+      
+      app.Run();
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# FileManager
+C# 및 JavaScript 파일 다운로드 및 업로드 서비스 토이 프로젝트
